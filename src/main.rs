e4d15e02c8beaf8ba3eae6f36533ee63abf38f07
chris <chris@layer8.org>
2023-05-18 15:53:49 -0400

    connection error handling

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
         ),
         NoTls,
     )
-    .unwrap();
+    .expect("Failed to connect to the database, ensure your .env file is correct");
 
     client
         .execute(
@@ -88,9 +88,9 @@ fn main() {
                 min_timestamp, max_timestamp, count
             ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             ON CONFLICT (uuid) DO UPDATE 
-            SET count = log_table.count + 1,
-                min_timestamp = LEAST(log_table.min_timestamp, EXCLUDED.min_timestamp),
-                max_timestamp = GREATEST(log_table.max_timestamp, EXCLUDED.max_timestamp)",
+            SET count = bgp_data.count + 1,
+                min_timestamp = LEAST(bgp_data.min_timestamp, EXCLUDED.min_timestamp),
+                max_timestamp = GREATEST(bgp_data.max_timestamp, EXCLUDED.max_timestamp)",
             &[
                 &uuid,
                 &elem_type,
