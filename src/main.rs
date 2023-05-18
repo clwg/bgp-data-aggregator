903f72081cc09c908f275283747b4e14fb3e6a15
chris <chris@layer8.org>
2023-05-18 15:28:47 -0400

    record map refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// Import necessary libraries and modules
 use bgpkit_parser::BgpkitParser;
 use dotenv::dotenv;
 use postgres::{Client, NoTls};
@@ -9,17 +8,12 @@ use uuid::Uuid;
 fn main() {
     dotenv().ok();
 
-    // Collect command line arguments
     let args: Vec<String> = env::args().collect();
-    // Get the URL from command line arguments
     let url = args.get(1).expect("Missing URL argument");
 
-    // Create a HashMap to store records and their associated data
-    let mut record_map: HashMap<String, (f64, f64, u32, Uuid)> = HashMap::new();
-
+    let mut record_map: HashMap<Uuid, (String, String, String, String, String, f64, f64, u32)> = HashMap::new();
     let namespace_uuid = Uuid::parse_str("40689d13-36ac-4216-9c41-f02b007d46c2").unwrap();
 
-    // Connect to PostgreSQL database
     let mut client = Client::connect(
         &format!(
             "host={} user={} password={} dbname={}",
@@ -33,13 +27,12 @@ fn main() {
     .unwrap();
 
     client
-    .execute(
-        &format!("SET search_path TO {}", env::var("PGSCHEMA").unwrap()),
-        &[],
-    )
-    .unwrap();
+        .execute(
+            &format!("SET search_path TO {}", env::var("PGSCHEMA").unwrap()),
+            &[],
+        )
+        .unwrap();
 
-    // Create table if not exists
     match client.execute(
         "CREATE TABLE IF NOT EXISTS log_table (
             uuid UUID PRIMARY KEY,
@@ -53,76 +46,64 @@ fn main() {
             count INTEGER DEFAULT 0
         );",
         &[],
-    ){
+    ) {
         Ok(_) => {}
         Err(err) => {
             eprintln!("Error creating table: {}", err);
         }
     }
 
-    // Iterate through elements returned by BgpkitParser
     for elem in BgpkitParser::new(url).unwrap() {
-        // Convert elem_type, as_path, next_hop, and peer_ip to strings
         let elem_type = format!("{:?}", elem.elem_type);
         let as_path = elem.as_path.as_ref().map(|p| p.to_string()).unwrap_or_default();
         let next_hop = elem.next_hop.map_or_else(|| String::new(), |ip| ip.to_string());
         let peer_ip = elem.peer_ip.to_string();
-
-        // Construct a key from the string representations
         let key = format!(
             "{}|{}|{}|{}|{}",
             elem_type, elem.prefix, as_path, next_hop, peer_ip
         );
-
-        // Generate the UUID v5 for the key
         let uuid5 = Uuid::new_v5(&namespace_uuid, key.as_bytes());
 
-        // Update or insert the min_timestamp, max_timestamp, count, and UUID v5 for the key in the record_map
-        let (min_timestamp, max_timestamp, count, _uuid) = record_map.entry(key.clone()).or_insert((
-            elem.timestamp,
-            elem.timestamp,
-            0,
-            uuid5,
-        ));
+        let (_elem_type, _prefix, _as_path, _next_hop, _peer_ip, min_timestamp, max_timestamp, count) = 
+            record_map.entry(uuid5).or_insert((
+                elem_type.clone(), 
+                elem.prefix.to_string(), 
+                as_path.clone(), 
+                next_hop.clone(), 
+                peer_ip.clone(), 
+                elem.timestamp, 
+                elem.timestamp, 
+                0,
+            ));
 
-        // Update min_timestamp and max_timestamp
         *min_timestamp = f64::min(*min_timestamp, elem.timestamp);
         *max_timestamp = f64::max(*max_timestamp, elem.timestamp);
-        // Increment count
         *count += 1;
     }
 
-    // Iterate through the record_map and output the final JSON objects
-    for (key, (min_timestamp, max_timestamp, count, uuid)) in record_map {
-        // Split the key into tokens for constructing the JSON object
-        let tokens: Vec<&str> = key.split('|').collect();
-    
-        // Insert into Postgres
+    for (uuid, (elem_type, prefix, as_path, next_hop, peer_ip, min_timestamp, max_timestamp, count)) in record_map {
         match client.execute(
             "INSERT INTO log_table (
-                elem_type, prefix, as_path, next_hop, peer_ip,
-                min_timestamp, max_timestamp, count, uuid
+                uuid, elem_type, prefix, as_path, next_hop, peer_ip,
+                min_timestamp, max_timestamp, count
             ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             ON CONFLICT (uuid) DO UPDATE 
             SET count = log_table.count + 1,
                 min_timestamp = LEAST(log_table.min_timestamp, EXCLUDED.min_timestamp),
                 max_timestamp = GREATEST(log_table.max_timestamp, EXCLUDED.max_timestamp)",
             &[
-                &tokens[0],
-                &tokens[1],
-                &tokens[2],
-                &tokens[3],
-                &tokens[4],
+                &uuid,
+                &elem_type,
+                &prefix,
+                &as_path,
+                &next_hop,
+                &peer_ip,
                 &(min_timestamp as f64),
                 &(max_timestamp as f64),
                 &(count as i32),
-                &uuid,
             ],
         ) {
-            Ok(_) => {
-                // Insertion successful
-                // Handle success if needed
-            }
+            Ok(_) => {}
             Err(err) => {
                 eprintln!("Error inserting record: {}", err);
             }
