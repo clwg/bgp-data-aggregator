a4554d4539c4be67774520e30588255aac868ae2
chris <chris@layer8.org>
2023-05-18 13:30:13 -0400

    datase insert

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 // Import necessary libraries and modules
 use bgpkit_parser::BgpkitParser;
-use serde_json::json;
+use dotenv::dotenv;
+use postgres::{Client, NoTls};
 use std::collections::HashMap;
 use std::env;
 use uuid::Uuid;
 
 fn main() {
+    dotenv().ok();
+
     // Collect command line arguments
     let args: Vec<String> = env::args().collect();
     // Get the URL from command line arguments
@@ -14,10 +17,45 @@ fn main() {
     // Create a HashMap to store records and their associated data
     let mut record_map: HashMap<String, (f64, f64, u32, Uuid)> = HashMap::new();
 
-    let namespace_uuid = Uuid::parse_str("40689d13-36ac-4216-9c41-f02b007d46c2").unwrap(); 
-    
-    // Namespace UUID for the UUID v5 generation
-    //let namespace_uuid = Uuid::NAMESPACE_DNS;
+    let namespace_uuid = Uuid::parse_str("40689d13-36ac-4216-9c41-f02b007d46c2").unwrap();
+
+    // Connect to PostgreSQL database
+    let mut client = Client::connect(
+        &format!(
+            "host={} user={} password={} dbname={}",
+            env::var("PGHOST").unwrap(),
+            env::var("PGUSER").unwrap(),
+            env::var("PGPASSWORD").unwrap(),
+            env::var("PGDATABASE").unwrap(),
+        ),
+        NoTls,
+    )
+    .unwrap();
+
+    client
+    .execute(
+        &format!("SET search_path TO {}", env::var("PGSCHEMA").unwrap()),
+        &[],
+    )
+    .unwrap();
+
+    // Create table if not exists
+    client
+        .execute(
+            "CREATE TABLE IF NOT EXISTS log_table (
+                elem_type VARCHAR(255),
+                prefix VARCHAR(255),
+                as_path VARCHAR(255),
+                next_hop VARCHAR(255),
+                peer_ip VARCHAR(255),
+                min_timestamp FLOAT,
+                max_timestamp FLOAT,
+                count INTEGER,
+                uuid UUID
+            );",
+            &[],
+        )
+        .unwrap();
 
     // Iterate through elements returned by BgpkitParser
     for elem in BgpkitParser::new(url).unwrap() {
@@ -55,22 +93,35 @@ fn main() {
     for (key, (min_timestamp, max_timestamp, count, uuid)) in record_map {
         // Split the key into tokens for constructing the JSON object
         let tokens: Vec<&str> = key.split('|').collect();
-
-        // Print the JSON object
-        println!(
-            "{}",
-            json!({
-                "elem_type": tokens[0],
-                "prefix": tokens[1],
-                "as_path": tokens[2],
-                "next_hop": tokens[3],
-                "peer_ip": tokens[4],
-                "min_timestamp": min_timestamp,
-                "max_timestamp": max_timestamp,
-                "count": count,
-                "uuid": uuid.to_string()
-            })
-            .to_string()
-        );
+    
+        // Insert into Postgres
+        match client.execute(
+            "INSERT INTO log_table (
+                elem_type, prefix, as_path, next_hop, peer_ip,
+                min_timestamp, max_timestamp, count, uuid
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+            &[
+                &tokens[0],
+                &tokens[1],
+                &tokens[2],
+                &tokens[3],
+                &tokens[4],
+                &(min_timestamp as f64),
+                &(max_timestamp as f64),
+                &(count as i32),
+                &uuid,
+            ],
+        ) {
+            Ok(_) => {
+                // Insertion successful
+                // Handle success if needed
+            }
+            Err(err) => {
+                // Error occurred during insertion
+                // Handle the error in an appropriate way
+                eprintln!("Error inserting record: {}", err);
+                // You can choose to log the error, retry the operation, or take other actions based on your requirements.
+            }
+        }
     }
 }
