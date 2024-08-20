fe961d1da2d3279653952bd8220387225a2ecb00
chris <chris@layer8.org>
2024-08-20 18:30:57 -0400

    refactor

--- Readme.md
+++ Readme.md
@@ -1,31 +1,25 @@
 # BGP Data Aggregator
 
-This Rust application takes a URL as an argument and processes Border Gateway Protocol (BGP) rib file (Routing Information Base), storing the result in a PostgreSQL database. The application utilizes bgpkit_parser to parse BGP data and postgres for database interaction. It uniquely identifies BGP data records based on a combination of data attributes and updates the records based on minimum and maximum timestamps.
+Analyzes BGP rib files and aggregates the data into a sqlite database, jsonl or csv files.
 
-## Requirements
-To build and run this project, you will need:
+## Building
 
-- Rust
-- PostgreSQL
-- An Internet connection (for fetching BGP data)
+`cargo build --release`
 
-## Setup
-1. Clone the repository - Clone this repository to your local machine.
-2. Environment Variables - Create a .env file in the root of the project, and provide the following environment variables, required for connecting to your PostgreSQL database:
-```
-PGHOST=<your-host>
-PGUSER=<your-username>
-PGPASSWORD=<your-password>
-PGDATABASE=<your-database>
-PGSCHEMA=<your-schema>
-```
-3. Build the project
-```
-cargo build --release     
-```
+## Usage
 
-## Running the Application
-To run the application, provide the URL of the BGP data as a command line argument. Here's an example:
-```
-./target/release/bgp_data_aggregator "http://archive.routeviews.org/bgpdata/2023.01/UPDATES/updates.20230101.1430.bz2"
-```
+First find the BGP data you want to analyze here http://archive.routeviews.org/bgpdata/
+
+Full RIB processing can be intensive, it is recommended that you have atleast 16GB of available RAM if you want to process the full RIB.
+
+Example usage:
+
+`./target/release/bgp_data_aggregator http://archive.routeviews.org/bgpdata/2024.08/UPDATES/updates.20240801.0030.bz2`
+
+The output format can be specified with a flag, the default is to a raw text file, but --json, --csv and --sqlite are also available.
+
+`./target/release/bgp_data_aggregator http://archive.routeviews.org/bgpdata/2024.08/UPDATES/updates.20240801.0030.bz2 --json`
+
+`./target/release/bgp_data_aggregator http://archive.routeviews.org/bgpdata/2024.08/UPDATES/updates.20240801.0030.bz2 --csv`
+
+`./target/release/bgp_data_aggregator http://archive.routeviews.org/bgpdata/2024.08/UPDATES/updates.20240801.0030.bz2 --sqlite`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use bgpkit_parser::BgpkitParser;
 use dotenv::dotenv;
-use postgres::{Client, NoTls};
+use ipnetwork::IpNetwork;
+use rusqlite::{params, Connection};
+use serde_json::json;
 use std::collections::HashMap;
 use std::env;
+use std::fs::File;
+use std::io::{Write, BufWriter};
 use uuid::Uuid;
+use std::net::IpAddr;
+use num_bigint::BigUint;
+use csv::Writer;
+
+fn ip_to_int(ip: IpAddr) -> BigUint {
+    match ip {
+        IpAddr::V4(addr) => BigUint::from(u32::from(addr)),
+        IpAddr::V6(addr) => BigUint::from_bytes_be(&addr.octets()),
+    }
+}
 
 fn main() {
     dotenv().ok();
@@ -11,64 +25,101 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let url = args.get(1).expect("Missing URL argument");
 
-    let mut record_map: HashMap<Uuid, (String, String, String, String, String, f64, f64, u32)> = HashMap::new();
-    let namespace_uuid = Uuid::parse_str("40689d13-36ac-4216-9c41-f02b007d46c2").unwrap();
+    // Command-line switches
+    let use_jsonl = args.contains(&"--jsonl".to_string());
+    let use_csv = args.contains(&"--csv".to_string());
+    let use_sqlite = args.contains(&"--sqlite".to_string());
 
-    let mut client = Client::connect(
-        &format!(
-            "host={} user={} password={} dbname={}",
-            env::var("PGHOST").unwrap(),
-            env::var("PGUSER").unwrap(),
-            env::var("PGPASSWORD").unwrap(),
-            env::var("PGDATABASE").unwrap(),
-        ),
-        NoTls,
-    )
-    .expect("Failed to connect to the database, ensure your .env file is correct");
+    // File output setup
+    let extension = if use_jsonl {
+        "jsonl"
+    } else if use_csv {
+        "csv"
+    } else {
+        "txt"
+    };
+    let filename = format!("{}_output.{}", url.split('/').last().unwrap_or("output"), extension);
+    
+    // Initialize output file or CSV writer
+    let mut file = if !use_sqlite && !use_csv {
+        Some(BufWriter::new(File::create(&filename).expect("Unable to create file")))
+    } else {
+        None
+    };
 
-    client
-        .execute(
-            &format!("SET search_path TO {}", env::var("PGSCHEMA").unwrap()),
-            &[],
+    let mut csv_writer = if use_csv {
+        let wtr = Writer::from_path(&filename).expect("Unable to create CSV file");
+        Some(wtr)
+    } else {
+        None
+    };
+
+    // SQLite setup
+    let conn = if use_sqlite {
+        let db_filename = String::from("bgp_db.sqlite");
+        let conn = Connection::open(db_filename).expect("Unable to open SQLite database");
+        conn.execute("PRAGMA synchronous = OFF", []).unwrap();
+
+        // Create table with asn column
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS bgp_data (
+                uuid TEXT PRIMARY KEY,
+                elem_type TEXT,
+                prefix TEXT,
+                start_ip BIGINT,
+                end_ip BIGINT,
+                as_path TEXT,
+                asn TEXT,
+                next_hop TEXT,
+                peer_ip TEXT,
+                min_timestamp INTEGER,
+                max_timestamp INTEGER,
+                count INTEGER DEFAULT 0
+            );",
+            [],
         )
-        .unwrap();
+        .expect("Failed to create table");
 
-    match client.execute(
-        "CREATE TABLE IF NOT EXISTS bgp_data (
-            uuid UUID PRIMARY KEY,
-            elem_type VARCHAR(255),
-            prefix INET,
-            as_path VARCHAR(255),
-            next_hop VARCHAR(255),
-            peer_ip VARCHAR(255),
-            min_timestamp FLOAT,
-            max_timestamp FLOAT,
-            count INTEGER DEFAULT 0
-        );",
-        &[],
-    ) {
-        Ok(_) => {}
-        Err(err) => {
-            eprintln!("Error creating table: {}", err);
-        }
-    }
+        Some(conn)
+    } else {
+        None
+    };
+
+    // Processing data
+    let mut record_map: HashMap<Uuid, (String, String, BigUint, BigUint, String, String, String, String, f64, f64, u32)> = HashMap::new();
+    let namespace_uuid = Uuid::parse_str("40689d13-36ac-4216-9c41-f02b007d46c2").unwrap();
 
     for elem in BgpkitParser::new(url).unwrap() {
         let elem_type = format!("{:?}", elem.elem_type);
         let as_path = elem.as_path.as_ref().map(|p| p.to_string()).unwrap_or_default();
+        let asn = as_path.split_whitespace().last().unwrap_or("").to_string();
         let next_hop = elem.next_hop.map_or_else(|| String::new(), |ip| ip.to_string());
         let peer_ip = elem.peer_ip.to_string();
+
+        // Parse the prefix to get the start and end IPs
+        let prefix = elem.prefix.to_string();
+        let ip_network: IpNetwork = prefix.parse().expect("Invalid IP network format");
+        let start_ip = ip_network.network();
+        let end_ip = ip_network.broadcast();
+
+        // Convert start_ip and end_ip to their integer representations
+        let start_ip_int = ip_to_int(start_ip);
+        let end_ip_int = ip_to_int(end_ip);
+
         let key = format!(
             "{}|{}|{}|{}|{}",
-            elem_type, elem.prefix, as_path, next_hop, peer_ip
+            elem_type, prefix, as_path, next_hop, peer_ip
         );
         let uuid5 = Uuid::new_v5(&namespace_uuid, key.as_bytes());
 
-        let (_elem_type, _prefix, _as_path, _next_hop, _peer_ip, min_timestamp, max_timestamp, count) = 
+        let (_elem_type, _prefix, _start_ip, _end_ip, _as_path, _asn, _next_hop, _peer_ip, min_timestamp, max_timestamp, count) = 
             record_map.entry(uuid5).or_insert((
                 elem_type.clone(), 
-                elem.prefix.to_string(), 
+                prefix.clone(), 
+                start_ip_int.clone(), 
+                end_ip_int.clone(), 
                 as_path.clone(), 
+                asn.clone(),
                 next_hop.clone(), 
                 peer_ip.clone(), 
                 elem.timestamp, 
@@ -81,32 +132,81 @@ fn main() {
         *count += 1;
     }
 
-    for (uuid, (elem_type, prefix, as_path, next_hop, peer_ip, min_timestamp, max_timestamp, count)) in record_map {
-        match client.execute(
-            "INSERT INTO bgp_data (
-                uuid, elem_type, prefix, as_path, next_hop, peer_ip,
-                min_timestamp, max_timestamp, count
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            ON CONFLICT (uuid) DO UPDATE 
-            SET count = bgp_data.count + 1,
-                min_timestamp = LEAST(bgp_data.min_timestamp, EXCLUDED.min_timestamp),
-                max_timestamp = GREATEST(bgp_data.max_timestamp, EXCLUDED.max_timestamp)",
-            &[
-                &uuid,
-                &elem_type,
-                &prefix,
-                &as_path,
-                &next_hop,
-                &peer_ip,
-                &(min_timestamp as f64),
-                &(max_timestamp as f64),
-                &(count as i32),
-            ],
-        ) {
-            Ok(_) => {}
-            Err(err) => {
-                eprintln!("Error inserting record: {}", err);
-            }
+    // CSV headers
+    if let Some(ref mut writer) = csv_writer {
+        writer.write_record(&["uuid", "element_type", "prefix", "start_ip", "end_ip", "as_path", "asn", "next_hop", "peer_ip", "min_timestamp", "max_timestamp", "count"])
+            .expect("Failed to write CSV headers");
+    }
+
+    // Output results
+    for (uuid, (elem_type, prefix, start_ip_int, end_ip_int, as_path, asn, next_hop, peer_ip, min_timestamp, max_timestamp, count)) in record_map {
+        if let Some(ref conn) = conn {
+            conn.execute(
+                "INSERT INTO bgp_data (uuid, elem_type, prefix, start_ip, end_ip, as_path, asn, next_hop, peer_ip, min_timestamp, max_timestamp, count)
+                VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12)
+                ON CONFLICT(uuid) DO UPDATE SET
+                count = count + 1,
+                min_timestamp = MIN(min_timestamp, excluded.min_timestamp),
+                max_timestamp = MAX(max_timestamp, excluded.max_timestamp)",
+                params![
+                    uuid.to_string(),
+                    elem_type,
+                    prefix,
+                    start_ip_int.to_string(),
+                    end_ip_int.to_string(),
+                    as_path,
+                    asn,
+                    next_hop,
+                    peer_ip,
+                    min_timestamp,
+                    max_timestamp,
+                    count,
+                ],
+            )
+            .expect("Error inserting record into SQLite");
+        } else if use_jsonl {
+            let record = json!({
+                "uuid": uuid.to_string(),
+                "elem_type": elem_type,
+                "prefix": prefix,
+                "start_ip": start_ip_int.to_string(),
+                "end_ip": end_ip_int.to_string(),
+                "as_path": as_path,
+                "asn": asn,
+                "next_hop": next_hop,
+                "peer_ip": peer_ip,
+                "min_timestamp": min_timestamp,
+                "max_timestamp": max_timestamp,
+                "count": count
+            });
+            writeln!(file.as_mut().unwrap(), "{}", record.to_string()).expect("Unable to write to file");
+        } else if use_csv {
+            csv_writer.as_mut().unwrap().write_record(&[
+                uuid.to_string(),
+                elem_type,
+                prefix,
+                start_ip_int.to_string(),
+                end_ip_int.to_string(),
+                as_path,
+                asn,
+                next_hop,
+                peer_ip,
+                min_timestamp.to_string(),
+                max_timestamp.to_string(),
+                count.to_string(),
+            ]).expect("Unable to write to CSV");
+        } else {
+            // Write plain text format with asn
+            writeln!(
+                file.as_mut().unwrap(),
+                "{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}",
+                uuid, elem_type, prefix, start_ip_int.to_string(), end_ip_int.to_string(), as_path, asn, next_hop, peer_ip, min_timestamp, max_timestamp, count
+            ).expect("Unable to write to file");
         }
+    }
+
+    // Finalize CSV
+    if let Some(ref mut writer) = csv_writer {
+        writer.flush().expect("Failed to finalize CSV file");
     }
 }
